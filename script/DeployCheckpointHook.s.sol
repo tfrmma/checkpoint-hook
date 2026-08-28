964decb3afbaf21dd0c7a693afc2dc5537396647
Taha MM <fadilrezokt@gmail.com>
2026-08-28 16:42:24 +0100

    Include minTickSpacing in DeployCheckpointHook
    Added minTickSpacing parameter to CheckpointHook deployment.

--- script/DeployCheckpointHook.s.sol
+++ script/DeployCheckpointHook.s.sol
@@ -16,6 +16,8 @@ import {CheckpointHook} from "../src/CheckpointHook.sol";
 //   GOVERNANCE_ADDRESS  owner of the deployed hook, use a multisig, not an EOA
 //   TREASURY_ADDRESS    fallback recipient for captured fees
 //   BLOCK_NUMBER_OFFSET optional, JIT window in blocks, defaults to 5
+//   MIN_TICK_SPACING    optional, floor on pools this hook can attach to, defaults to 60
+//                       (Uniswap's medium fee tier spacing), see minTickSpacing on the contract
 contract DeployCheckpointHook is Script {
     // Arachnid's deterministic deployment proxy, forge script routes any new X{salt: ...}()
     // through this when broadcasting. Same address on every chain that has it deployed.
@@ -26,16 +28,18 @@ contract DeployCheckpointHook is Script {
         address governance = vm.envAddress("GOVERNANCE_ADDRESS");
         address treasury = vm.envAddress("TREASURY_ADDRESS");
         uint48 blockNumberOffset = uint48(vm.envOr("BLOCK_NUMBER_OFFSET", uint256(5)));
+        uint24 minTickSpacing = uint24(vm.envOr("MIN_TICK_SPACING", uint256(60)));
 
         IPoolManager poolManager = IPoolManager(poolManagerAddress);
 
         uint160 flags = uint160(
-            Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_SWAP_FLAG | Hooks.AFTER_SWAP_RETURNS_DELTA_FLAG
-                | Hooks.AFTER_ADD_LIQUIDITY_FLAG | Hooks.AFTER_REMOVE_LIQUIDITY_FLAG
+            Hooks.BEFORE_INITIALIZE_FLAG | Hooks.BEFORE_SWAP_FLAG | Hooks.AFTER_SWAP_FLAG
+                | Hooks.AFTER_SWAP_RETURNS_DELTA_FLAG | Hooks.AFTER_ADD_LIQUIDITY_FLAG | Hooks.AFTER_REMOVE_LIQUIDITY_FLAG
                 | Hooks.AFTER_ADD_LIQUIDITY_RETURNS_DELTA_FLAG | Hooks.AFTER_REMOVE_LIQUIDITY_RETURNS_DELTA_FLAG
         );
 
-        bytes memory constructorArgs = abi.encode(poolManager, blockNumberOffset, governance, treasury);
+        bytes memory constructorArgs =
+            abi.encode(poolManager, blockNumberOffset, governance, treasury, minTickSpacing);
         (address predictedAddress, bytes32 salt) =
             HookMiner.find(CREATE2_DEPLOYER, flags, type(CheckpointHook).creationCode, constructorArgs);
 
@@ -43,7 +47,7 @@ contract DeployCheckpointHook is Script {
         console2.log("salt:", vm.toString(salt));
 
         vm.startBroadcast();
-        hook = new CheckpointHook{salt: salt}(poolManager, blockNumberOffset, governance, treasury);
+        hook = new CheckpointHook{salt: salt}(poolManager, blockNumberOffset, governance, treasury, minTickSpacing);
         vm.stopBroadcast();
 
         // if this trips, either the flags computed above drifted from getHookPermissions() or
@@ -51,9 +55,10 @@ contract DeployCheckpointHook is Script {
         require(address(hook) == predictedAddress, "address mismatch, salt didn't mine right");
 
         console2.log("deployed at:", address(hook));
-        console2.log("  poolManager =", address(poolManager));
-        console2.log("  governance  =", governance);
-        console2.log("  treasury    =", treasury);
-        console2.log("  blockOffset =", blockNumberOffset);
+        console2.log("  poolManager    =", address(poolManager));
+        console2.log("  governance     =", governance);
+        console2.log("  treasury       =", treasury);
+        console2.log("  blockOffset    =", blockNumberOffset);
+        console2.log("  minTickSpacing =", minTickSpacing);
     }
 }
